99244bd95e3609720748b1b4c72647be6b83aaea
stephenyoon84 <stephenyoon84@gmail.com>
2019-03-20 22:19:38 -0400

    add solution without render portion

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,7 @@
 class Song < ApplicationRecord
   validates :title, :artist_name, presence: true
-  validates :title, uniqueness: { scope: :release_year}
+  validates :title, uniqueness: { scope: %i[release_year artist_name]}
+  validates :released, inclusion: { in: [true, false] }
   validates :release_year, presence: true, if: :released
   validate :release_year_cannot_be_in_the_future
 
@@ -11,4 +12,15 @@ class Song < ApplicationRecord
       end
     end
   end
+
+  # with_options if: :released? do |song|
+  #   song.validates :release_year, presence: true
+  #   song.validates :release_year, numericality: {
+  #     less_than_or_equal_to: Date.today.year
+  #   }
+  # end
+  #
+  # def released?
+  #   released
+  # end
 end
